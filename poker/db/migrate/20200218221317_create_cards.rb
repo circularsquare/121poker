208de2b741682d20522572adab95b45736fd82cb
Andrew <andrewxinchen@gmail.com>
2020-02-18 16:55:29 -0800

    added card form and card model

---
+++ poker/db/migrate/20200218221317_create_cards.rb
@@ -0,0 +1,12 @@
+# file made by andrew while doing tutorial, run rails db:migrate to generate a table of cards?
+
+class CreateCards < ActiveRecord::Migration[6.0]
+  def change
+    create_table :cards do |t|
+      t.string :suit
+      t.string :rank
+
+      t.timestamps
+    end
+  end
+end
