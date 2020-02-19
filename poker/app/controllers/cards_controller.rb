31eb811f7b58aa5b866fdaa6baa878a10ff13ee4
Andrew <andrewxinchen@gmail.com>
2020-02-18 16:55:29 -0800

    added card form and card model

--- poker/app/controllers/cards_controller.rb
+++ poker/app/controllers/cards_controller.rb
@@ -4,4 +4,18 @@ class CardsController < ApplicationController
   def new
   end
 
+  def create
+    @card = Card.new(card_params) #generates card model from params of form
+    @card.save #saves to model in database
+    redirect_to @card
+  end
+
+  def show
+    @card = Card.find(params[:id])
+  end
+
+  private
+    def card_params
+      params.require(:card).permit(:rank, :suit)
+    end
 end
