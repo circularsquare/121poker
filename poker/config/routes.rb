d015b9146c895e1c31834d849a24589501bf9a3d
Andrew <andrewxinchen@gmail.com>
2020-02-21 17:08:43 -0800

    cleaned up webpages and added links between them

--- poker/config/routes.rb
+++ poker/config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :cards
   #this is a resources method which is used to declare a standard REST resource
 
+  get 'welcome/tutorial'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
