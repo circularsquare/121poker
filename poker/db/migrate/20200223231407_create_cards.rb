f43cc3ab36c099bb3fa8627c4c2d53aeb7c86500
Andrew <andrewxinchen@gmail.com>
2020-02-24 10:37:46 -0800

    refactor(migrations and models): restructure database

--- poker/db/migrate/20200223231407_create_cards.rb
+++ poker/db/migrate/20200223231407_create_cards.rb
@@ -1,12 +1,10 @@
-# file made by andrew while doing tutorial, run rails db:migrate to generate a table of cards?
-
 class CreateCards < ActiveRecord::Migration[6.0]
   def change
     create_table :cards do |t|
-      t.integer :card_id
       t.string :suit
       t.string :rank
       t.string :location
+      t.references :game, null: false, foreign_key: true
       t.timestamps
     end
   end
