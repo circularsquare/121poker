ed5cefb45a0a35658f82998565675c51c4947667
Andrew <andrewxinchen@gmail.com>
2020-02-24 18:24:29 -0800

    refactor(database): updated cards table to have more informative and flexible columns

--- poker/config/routes.rb
+++ poker/config/routes.rb
@@ -5,11 +5,14 @@ Rails.application.routes.draw do
   root 'welcome#index'
   #root means rails maps requests to the root (localhost:3000) to the index action
 
-  #created by andrew while learning rails 2/13, see app/controllers/cards_controller.rb
-  resources :games
-  #this is a resources method which is used to declare a standard REST resource
-
+  #extra webpages not associated with resources
   get 'welcome/tutorial'
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #created by andrew while learning rails 2/13, see app/controllers/cards_controller.rb
+  resources :games
+  #resources method, used to declare a standard REST resource, provides many routes
+  resources :cards
+  resources :users
+  resources :players
+
 end
