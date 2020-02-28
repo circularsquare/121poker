2d5c040a06dc1d5cbfaa2efb1ee7d81d0e4eeccc
Andrew <andrewxinchen@gmail.com>
2020-02-28 10:57:50 -0800

    restructure(database): made relationships between data tables

--- poker/app/models/game.rb
+++ poker/app/models/game.rb
@@ -1,2 +1,20 @@
 class Game < ApplicationRecord
+  has_many :players
+  has_many :cards
+
+  def deal_cards
+    deck = Card.all
+    13.times do
+      4.times do |i|
+        random_card = deck[rand(deck.length)]
+        deck.delete(random_card)
+      end
+    end
+  end
+
+  def change_position
+    @card = Card.find(1)
+    @card.location = 'bruh'
+  end
+
 end
--- poker/db/seeds.rb
+++ poker/db/seeds.rb
@@ -1,9 +1,17 @@
+# run rake db:seed to run this file
+
+# refreshes db (except users!)
+Game.delete_all
+Player.delete_all
+Card.delete_all
+
+
 ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 suits = ['S', 'H', 'C', 'D']
 
 # spawns the 52 cards for the game on database start
 ranks.collect do |rank|
   suits.collect do |suit|
-    Card.create({:rank => rank, :suit => suit, :location => 'deck'})
+    card = Card.create({:rank => rank, :suit => suit, :location => 'deck'})
   end
 end
