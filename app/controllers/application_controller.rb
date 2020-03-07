561420d7f58f49135958576fa80a5b8f5f473762
Andrew <andrewxinchen@gmail.com>
2020-03-06 21:39:40 -0800

    feat(routes): added routes for player adding methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  skip_forgery_protection
   helper_method :current_user
   helper_method :logged_in?
 
@@ -8,5 +9,5 @@ class ApplicationController < ActionController::Base
   def logged_in?
       !current_user.nil?
   end
-  
+
 end
