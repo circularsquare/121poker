844fdcdc02c8e526f17b5156bded3f8a67d76348
Andrew <andrewxinchen@gmail.com>
2020-03-06 22:38:38 -0800

    feat(cards): cards of games are now displayed on their gamepages

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,17 @@ class Game < ApplicationRecord
   has_many :players
   has_many :cards
 
+  def init
+    ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+    suits = ['S', 'H', 'C', 'D']
+    ranks.collect do |rank|
+      suits.collect do |suit|
+        self.cards << Card.new({:game_id => self.id, :rank => rank, :suit => suit, :location => 'deck'})
+        self.save
+      end
+    end
+  end
+
   # to implement: modify location of card to player
   # currently: removes cards from deck
   def deal_cards
@@ -21,14 +32,16 @@ class Game < ApplicationRecord
 
   # creates player under the current game and the specified user
   def add_user_player(user)
-    @player = Player.new({:ai => '', :game_id => self.id, :user_id => user.id, :money => 0, :username => user.username, :location => 'new player'})
+    @player = Player.new({:ai => '', :game_id => self.id, :user_id => user.id, :money => 0, :username => user.username, :location => 1})
+    @open_location += 1
     self.players << @player
     self.save
   end
 
   # creates ai player under the current game and the user who made it
   def add_ai_player(type, user)
-    @player = Player.new({:ai => type, :game_id => self.id, :user_id => user.id, :money => 0, :username => 'ai 1', :location => 'new player'})
+    @player = Player.new({:ai => type, :game_id => self.id, :user_id => user.id, :money => 0, :username => 'ai 1', :location => 1})
+    @open_location += 1
     self.players << @player
     self.save
   end
