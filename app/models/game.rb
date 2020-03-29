23c5115cd6923d59c3370be6208a9538f10ca242
andrew chen <andrewxinchen@gmail.com>
2020-03-29 15:02:58 -0400

    feat(game loop): ai actions happen after player actions do

--- app/models/game.rb
+++ app/models/game.rb
@@ -31,7 +31,9 @@ class Game < ApplicationRecord
         move_card(get_random_card, player.location)
         move_card(get_random_card, player.location)
         player.in_hand = true
+        player.save
       end
+      self.current_player = get_next_player(self.dealer)
     when 1
       move_card(get_random_card, 0)  #moves cards to table
       move_card(get_random_card, 0)
@@ -52,9 +54,30 @@ class Game < ApplicationRecord
     end
     self.high_bet = 0
     self.save
-
+    action_loop()
   end
 
+  def action_loop()
+
+
+    self.high_better = self.current_player
+    go_once_around = true
+    while (not self.high_better == self.current_player) or go_once_around
+      go_once_around = false
+      player = get_player(self.current_player)
+      if player.ai != ""
+        action_info = ai_action(player)
+        action(action_info[0], action_info[1], player.id)
+      end
+      self.current_player = get_next_player(self.current_player)
+
+      if self.players.where(:in_hand => true).length == 1
+        return 0
+      end
+    end
+    self.save
+  end
+  #handles player actions
   def action(type, amount, player)
     @player = Player.find(player.to_i)
     amount = amount.to_i
@@ -73,6 +96,7 @@ class Game < ApplicationRecord
         @player.money -= amount
         self.pot += amount
         self.high_bet = amount + @player.in_pot_current
+        self.high_better = player
       end
     when 'raise'
       if amount > @player.money
@@ -84,6 +108,7 @@ class Game < ApplicationRecord
         @player.money -= amount
         self.pot += amount
         self.high_bet = amount + @player.in_pot_current
+        self.high_better = player
       end
     when 'check'
     when 'call'
@@ -99,9 +124,21 @@ class Game < ApplicationRecord
     end
     @player.in_pot_current += amount
     @player.in_pot_hand += amount
+
     self.save
     @player.save
   end
+
+  def ai_action(player)
+    type = player.ai
+    if self.high_bet < player.money
+      return 'fold', 1
+    else
+      return 'fold', 1
+    end
+
+  end
+
   # locations are deck, table, or players
   def move_card(card, destination)
     card.location = destination
@@ -119,7 +156,6 @@ class Game < ApplicationRecord
       player.in_pot_hand = 0
     end
     # reset pot to empty, increment dealer
-    p 'Got here resetting game'
     self.pot = 0
     self.dealer = (self.dealer + 1)%10 + 10
     while self.players.where(:location => self.dealer).length == 0
@@ -134,6 +170,9 @@ class Game < ApplicationRecord
 
   def set_round(round)
     self.round = round
+    if round == 4
+      deal(4)
+    end
     self.save
   end
 
@@ -142,10 +181,15 @@ class Game < ApplicationRecord
     self.players.find(player.to_i).destroy
     self.save
   end
-
   # creates player under the current game and the specified user
   def add_user_player(user)
     @player = Player.new({:ai => '', :game_id => self.id, :user_id => user.id, :money => 100, :username => user.username, :location => self.get_empty_location(), :in_pot_hand => 0, :in_pot_current => 0})
+    self.players << @player
+    self.save
+  end
+  # creates ai player under the current game and the user who made it
+  def add_ai_player(type, user)
+    @player = Player.new({:ai => type, :game_id => self.id, :user_id => user.id, :money => 100, :username => 'ai 1', :location => self.get_empty_location(), :in_pot_hand => 0, :in_pot_current => 0})
     self.players << @player
     self.save
   end
@@ -158,12 +202,19 @@ class Game < ApplicationRecord
       end
     end
   end
-
-  # creates ai player under the current game and the user who made it
-  def add_ai_player(type, user)
-    @player = Player.new({:ai => type, :game_id => self.id, :user_id => user.id, :money => 0, :username => 'ai 1', :location => self.players.length+10})
-    self.players << @player
-    self.save
+  def get_player(location)
+    return self.players.where(:location => location)[0]
   end
-
+  def get_next_player(start)
+    for i in 1..10
+      to_check = (start + i)%10 + 10
+      if self.players.where(:location => to_check).length == 1
+        player = self.players.where(:location => to_check)[0]
+        if player.in_hand
+          return player.location
+        end
+      end
+    end
+    return -10
+  end
 end
